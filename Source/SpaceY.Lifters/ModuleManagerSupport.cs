138b180d741dd214dccec2c239a55a773daa5244
Lisias T <me@lisias.net>
2023-02-10 21:08:27 -0300

    Solving the duplicated `CUSTOM_PARTLIST_CATEGORY` that happens when the user creates their own custom categories.
    Fixes https://github.com/zer0Kerbal/SpaceYLifters/issues/106

---
+++ Source/SpaceY.Lifters/ModuleManagerSupport.cs
@@ -0,0 +1,41 @@
+﻿/*
+	This file is part of SpaceY.Lifters, and is
+		©2023 LisiasT : http://lisias.net <support@lisias.net>
+
+	SpaceY.Lifters is
+		© 2022-2023 zer0Kerbal
+		© 2014-2022 Necrobones
+
+	and licensed as follows:
+		* CC-BY-NC-SA-4.0 : https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+	SpaceY.Lifters is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceY.Lifters
+{
+	public class ModuleManagerSupport : MonoBehaviour
+	{
+		public static IEnumerable<string> ModuleManagerAddToModList()
+		{
+			List<string> list = new List<string>();
+			{
+				ConfigNode[] cns = GameDatabase.Instance.GetConfigNodes("CUSTOM_PARTLIST_CATEGORY");
+				bool found = false;
+				foreach (ConfigNode cn in cns) if (cn.HasValue("categoryName") && "SpaceY".Equals(cn.GetValue("categoryName")))
+				{
+					Debug.Log("[SpaceY.Lifters] CUSTOM_PARTLIST_CATEGORY for SpaceY already found. Avoiding creating it again.");
+					found = true;
+					break;
+				}
+				if (!found) list.Add("SpaceYLifters_Category");	
+			}
+			string[] r = list.ToArray();
+			return r;
+		}
+	}
+}
